25213e9ac0721300fac47893144bf6647237f874
OGoncharenko <olyas.goncharenko@gmail.com>
2025-02-26 00:55:25 -0800

    Update cookie settings in generateTokenAndSetCookie.js to allow cross-site requests

--- backend/utils/generateTokenAndSetCookie.js
+++ backend/utils/generateTokenAndSetCookie.js
@@ -11,9 +11,10 @@ export const generateTokenAndSetCookie = (res, userId) => {
   res.cookie('token', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
     maxAge: 1000 * 60 * 60 * 24 * 7,
-    // domain: process.env.COOKIE_DOMAIN,
+    domain: process.env.COOKIE_DOMAIN,
+    sameSite: "none",
+    path: "/",
   });
   return token;
 }
