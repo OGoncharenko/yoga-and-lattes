6332d2517ec2a0123417779bef399ec58ff02c04
OGoncharenko <olyas.goncharenko@gmail.com>
2025-02-26 01:02:25 -0800

    Update cookie settings in generateTokenAndSetCookie.js to allow cross-site requests

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({origin: "https://yoga-and-lattes-frontend.onrender.com" || "http://localhost:5173", credentials: true}));
+app.use(cors({origin: process.env.CLIENT_URL || "http://localhost:5173", credentials: true}));
 app.use(express.json());
 app.use(cookieParser());
 
--- backend/utils/generateTokenAndSetCookie.js
+++ backend/utils/generateTokenAndSetCookie.js
@@ -12,7 +12,7 @@ export const generateTokenAndSetCookie = (res, userId) => {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
     maxAge: 1000 * 60 * 60 * 24 * 7,
-    domain: ".onrender.com",
+    domain: process.env.COOKIE_DOMAIN,
     sameSite: "none",
     path: "/",
   });
