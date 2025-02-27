ff7188c1c463e5b8f399894e8ec4e16b3094cd1c
OGoncharenko <olyas.goncharenko@gmail.com>
2025-02-27 11:25:11 -0800

    Fetch and display featured posts with dynamic data in FeaturedPosts component

--- frontend/src/components/FeaturedPosts.jsx
+++ frontend/src/components/FeaturedPosts.jsx
@@ -1,83 +1,103 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import axios from "axios";
+import {useQuery} from "@tanstack/react-query";
+
+const fetchPosts = async () => {
+  console.log('Fetching posts from:', `${import.meta.env.VITE_API_URL}/posts?featured=true&limit=4&sort=newest`);
+  const response = await axios.get(`${import.meta.env.VITE_API_URL}/posts?featured=true&limit=4&sort=newest`);
+  console.log('API response:', response.data);
+  return response.data;
+}
 
 const FeaturedPosts = () => {
+  const {isPending, error, data} = useQuery({
+    queryKey: ['featuredPosts'],
+    queryFn: () => fetchPosts(),
+  });
+
+  const posts = data;
+  if (isPending) return "Loading...";
+  if (error) return "An error has occurred: " + error.message;
+  if(!posts || data.length === 0) return;
+
+
   return (
     <div className='mt-8 flex flex-col lg:flex-row gap-8'>
       {/*first*/}
       <div className='w-full lg:w-1/2 flex flex-col gap-4'>
         {/*image*/}
-        <img
-          src='/IMG_8729.JPG'
+        {posts[0].img && <img
+          src={posts[0].img}
           alt='placeholder'
           className='rounded-3xl object-cover'
-        />
+        />}
         {/*details*/}
         <div className='flex items-center gap-4'>
           <h1 className='font-semibold lg:text-lg'>01.</h1>
-          <Link className='text-blue-800 lg:text-lg'>Hatha yoga</Link>
+          <Link className='text-blue-800 lg:text-lg'>{posts[0].category}</Link>
         </div>
         {/*title*/}
-        <Link to="/christmas-scenario" className='text-xl lg:text-3xl font-semibold lg:font-bold'>
-          The Best Yoga Scenarios for the Christmas Holidays
+        <Link to={posts[0].slug} className='text-xl lg:text-3xl font-semibold lg:font-bold'>
+          {posts[0].title}
         </Link>
       </div>
       {/*others*/}
       <div className='w-full lg:w-1/2 flex flex-col gap-4'>
         {/*second*/}
         <div className='lg:h-1/3 flex justify-between gap-4'>
-            <img
-              src='/find_your_way.jpeg' alt='placeholder'
-              className='w-1/3 rounded-3xl object-cover aspect-video h-full'
-            />
+          {posts[1].img && <img
+            src={posts[1].img} alt='placeholder'
+            className='w-1/3 rounded-3xl object-cover aspect-video h-full'
+          />}
           {/*details and title*/}
           <div className='w-2/3'>
             {/*details*/}
             <div className='flex items-center gap-4 text-sm lg:text-base mb-4'>
               <h1 className='font-semibold'>02.</h1>
-              <Link className='text-blue-800'>Mindset</Link>
+              <Link className='text-blue-800'>{posts[1].category}</Link>
             </div>
             {/*title*/}
-            <Link to="/find-your-way" className='text-base sm:text-lg md:text-2xl lg:text-xl xl:text-2xl font-medium'>
-              How to find your way in yoga and meditation
+            <Link to={posts[1].slug} className='text-base sm:text-lg md:text-2xl lg:text-xl xl:text-2xl font-medium'>
+              {posts[1].title}
             </Link>
           </div>
         </div>
         {/*third*/}
         <div className='lg:h-1/3 flex justify-between gap-4'>
-          <img
-            src='/zen-stones_163782-9216%20(1).jpg'
+          {posts[2].img && <img
+            src={posts[2].img}
             alt='placeholder'
             className='w-1/3 rounded-3xl object-cover aspect-video h-full'
-          />
+          />}
           <div className='w-2/3'>
             {/*details*/}
             <div className='flex items-center gap-4 text-sm lg:text-base mb-4'>
               <h1 className='font-semibold'>03.</h1>
-              <Link className='text-blue-800'>Meditation</Link>
+              <Link className='text-blue-800'>{posts[2].category}</Link>
             </div>
             {/*title*/}
-            <Link to="/benefits-meditation" className='text-base sm:text-lg md:text-2xl lg:text-xl xl:text-2xl font-medium'>
-              The Benefits of Meditation for the Body and Mind
+            <Link to={posts[2].slug} className='text-base sm:text-lg md:text-2xl lg:text-xl xl:text-2xl font-medium'>
+              {posts[2].title}
             </Link>
           </div>
         </div>
         {/*fourth*/}
         <div className='lg:h-1/3 flex justify-between gap-4'>
-          <img
-            src='/yoga_nature.jpg'
+          {posts[3].img && <img
+            src={posts[3].img}
             alt='placeholder'
             className='w-1/3 rounded-3xl object-cover aspect-video h-full'
-          />
+          />}
           <div className='w-2/3'>
             {/*details*/}
             <div className='flex items-center gap-4 text-sm lg:text-base mb-4'>
               <h1 className='font-semibold'>04.</h1>
-              <Link className='text-blue-800'>Asanas</Link>
+              <Link className='text-blue-800'>{posts[3].category}</Link>
             </div>
             {/*title*/}
-            <Link to="/test" className='text-base sm:text-lg md:text-2xl lg:text-xl xl:text-2xl font-medium'>
-              Asanas for Beginners: The Best Yoga Poses to Start With
+            <Link to={posts[3].slug} className='text-base sm:text-lg md:text-2xl lg:text-xl xl:text-2xl font-medium'>
+              {posts[3].title}
             </Link>
           </div>
         </div>
