5abd0250638100a668f8456807380d8662d2d89c
OGoncharenko <olyas.goncharenko@gmail.com>
2025-03-05 18:25:47 -0800

    Add tests for posts with CRUD

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -4,7 +4,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI)
+    // let mongoURL = process.env.MONGO_URI;
+    // if (process.env.NODE_ENV === "test") {
+    //   mongoURL = process.env.MONGO_URI_TEST;
+    // }
+
+    const mongoUri = process.env.NODE_ENV === 'test' ? process.env.MONGO_URI_TEST : process.env.MONGO_URI;
+    await mongoose.connect(mongoUri)
     console.log('Connected to MongoDB')
   } catch(err) {
     console.log(err)
--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ import commentRouter from "./routes/comment.route.js";
 
 dotenv.config();
 
-const app = express();
+export const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({origin: process.env.CLIENT_URL || "http://localhost:5173", credentials: true}));
